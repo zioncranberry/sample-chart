5e1be588a5c9a2a8c0ee5f2f3b32f816de332868
Jane Nerizon <janenerizon93@gmail.com>
2023-12-28 23:19:56 +0800

    v1

---
+++ story_content/user.js
@@ -0,0 +1,31 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6pog4flCBAo":
+        Script1();
+        break;
+      case "6WEzF8lZNEO":
+        Script2();
+        break;
+      case "6hvVYXpRv9y":
+        Script3();
+        break;
+  }
+}
+
+function Script1()
+{
+  document.getElementById('volume').style.display = 'none';
+}
+
+function Script2()
+{
+  document.getElementById('volume').style.display = 'none';
+}
+
+function Script3()
+{
+  document.getElementById('volume').style.display = 'none';
+}
+
